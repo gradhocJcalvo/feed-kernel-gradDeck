11259ddede1f88fef8aeb06dd497cbffebb0eae8
gradhoc-jcalvo <jcalvo@gradhoc.com>
2025-09-24 13:10:01 +0200

    feed openwrt kernel

--- target/linux/stm32/image/demo.mk
+++ target/linux/stm32/image/demo.mk
@@ -30,38 +30,13 @@ define Device/Demo-stm32mp2
 		     usbgadget
 endef
 
-define Device/stm32mp157f-dk2-demo
-  $(call Device/stm32mp157f-dk2)
-  $(call Device/Demo)
-  DEVICE_NAME := stm32mp157f-dk2
-endef
-
-define Device/stm32mp135f-dk-demo
-  $(call Device/stm32mp135f-dk)
-  $(call Device/Demo)
-  DEVICE_NAME := stm32mp135f-dk
-endef
-
 define Device/stm32mp257f-ev1-demo
   $(call Device/stm32mp257f-ev1)
   $(call Device/Demo-stm32mp2,stm32mp257f-ev1)
   DEVICE_NAME := stm32mp257f-ev1
 endef
 
-define Device/stm32mp257f-dk-demo
-  $(call Device/stm32mp257f-dk)
-  $(call Device/Demo-stm32mp2,stm32mp257f-dk)
-  DEVICE_NAME := stm32mp257f-dk
-endef
-
-ifeq ($(SUBTARGET),stm32mp1)
-  TARGET_DEVICES += \
-		    stm32mp157f-dk2-demo \
-		    stm32mp135f-dk-demo
-endif
-
 ifeq ($(SUBTARGET),stm32mp2)
   TARGET_DEVICES += \
-		    stm32mp257f-ev1-demo \
-		    stm32mp257f-dk-demo
+		    stm32mp257f-ev1-demo
 endif
